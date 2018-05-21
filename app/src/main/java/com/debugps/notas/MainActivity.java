b359bb494f34d75b42c894d319be9454a62c7ee6
Douglas_HdezT <00004516@uca.edu.sv>
2018-05-21 08:22:23 -0600

    Main UI

--- app/src/main/java/com/debugps/notas/MainActivity.java
+++ app/src/main/java/com/debugps/notas/MainActivity.java
@@ -1,14 +1,45 @@
 package com.debugps.notas;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.debugps.notas.fragments.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private DrawerLayout mDrawerLayout;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+
+        NavigationView mNavigationView = findViewById(R.id.nav_layout);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        fragmentTransaction.replace(R.id.main_frame_layout, new MainFragment());
+
+        fragmentTransaction.commit();
+
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                item.setChecked(true);
+                mDrawerLayout.closeDrawers();
+
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/debugps/notas/fragments/MainFragment.java
@@ -0,0 +1,24 @@
+package com.debugps.notas.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.debugps.notas.R;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.main_fragment_layout, container, false);
+        return view;
+    }
+}
